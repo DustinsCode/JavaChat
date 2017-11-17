faaba4d33040d2c2210d31ad558d9c55e69e3a4a
RyanWalt <waltr@mail.gvsu.edu>
2017-11-17 17:45:16 -0500

    Changes to the server..

--- Server.java
+++ Server.java
@@ -64,7 +64,7 @@ public class Server{
                 message.trim();
 
                 //check if message is a command...
-                if (!isCommand(message)){
+                if (!isCommand(userName, sc, message)){
                     //Else send it to all...
                     message = userName + ": " + message.trim();
                     System.out.println(message);
@@ -94,36 +94,82 @@ public class Server{
         return sb.toString();
     }
 
-    public boolean isCommand(String m){
-        String[] contents = m.split(" ")
+    public boolean isCommand(String senderName, SocketChannel sender, String m){
+        String[] contents = m.split(" ");
 
-        switch(contents[0]):
+        switch(contents[0]){
             case "/exit":
-                exit(contents[1]);
+                exit(sender);
                 return true;
             case "/pm":
+                //Get the message of the user and turn it back to a string.
+                String temp = "";
+                for (int i = 2; i < contents.length; i++){
+                    temp += contents[i] + " ";
+                }
+                temp = temp.trim();
+                //Send the message to a specific user.
+                sendPM(senderName, sender, contents[1], temp);
                 return true;
             case "/kick":
+                System.out.println("User to kick: " + contents[1]);
+                kick(sender, contents[1]);
                 return true;
+        }
 
         return false;
     }
 
-    public void sendPM(){
-
-    }
-
-	public void kick(){
-
-	}
-
-	public void exit(String user){
-		for(key i: allUsers.keys()){
-			if i.equals(user){
-				allUsers.get(i).close();
-				allUsers.remove(i);
+    public void sendPM(String sender, SocketChannel s, String user, String m){
+        for(String i: allUsers.keySet()){
+			if(i.equals(user)){
+				String message = "PM from " + sender + ": " + m;
+				try{
+					ByteBuffer buff = ByteBuffer.wrap(message.getBytes());
+					allUsers.get(i).write(buff);
+					message = "PM to " + user + ": " + m;
+					buff = ByteBuffer.wrap(message.getBytes());
+					s.write(buff);
+				}catch(Exception e){
+					System.out.println("Error sending PM: " + e);
+				}
 			}
 		}
+		return;
+    }
+
+	public void kick(SocketChannel s, String user){
+		//sendPM(user, "You have been kicked by an admin. Deuces.");
+        user = user.trim();
+		for(String i: allUsers.keySet()){
+            System.out.println(i.equals(user));
+			if(i.equals(user)){
+                System.out.println("HIT");
+				SocketChannel kickee = allUsers.get(i);
+				ByteBuffer b = ByteBuffer.wrap("You have been kicked by an admin.".getBytes());
+				try{
+
+					kickee.write(b);
+					kickee.close();
+                    System.out.println("Closed: " + user);
+				}catch(Exception e){
+					System.out.println("Error kicking user: " + e);
+				}
+				allUsers.remove(i);
+                break;
+			}
+		}
+		return;
+	}
+
+	public void exit(SocketChannel user){
+        System.out.println("exiting...");
+		try{
+			user.close();
+		}catch(Exception e){
+			System.out.println("Error on exit: " + e);
+		}
+		return;
 	}
 
     public void sendMessageAll(ByteBuffer b){
